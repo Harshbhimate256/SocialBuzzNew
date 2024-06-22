d9d18abdc8a0b823b3887eae9eb1f321404ece7c
Harsh Bhimate <harshbhimate2018@gmail.com>
2024-06-22 21:35:26 +0530

    responsive

---
+++ public/javascripts/comment-Section.js
@@ -0,0 +1,22 @@
+const toggleBtn = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+const menuIcon = document.getElementById('menu');
+const closeIcon = document.getElementById('close');
+const main = document.getElementById('main')
+toggleBtn.addEventListener("click",()=>{
+    if(nav.style.left === '0px'){
+        toggleBtn.style.backgroundColor = 'lightblue';
+        nav.style.left = "-100%";
+        closeIcon.style.display = 'none';
+        menuIcon.style.display = 'block';
+        main.style.opacity = 1;
+    }
+    else{
+        nav.style.left = '0px';
+        toggleBtn.style.zIndex = 100;
+        closeIcon.style.display = 'block';
+        toggleBtn.style.backgroundColor = 'white';
+        menuIcon.style.display = 'none';
+        main.style.opacity = .5;
+    }
+});
